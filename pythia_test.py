686ae93a33c0f686474799b632eec77375a087f5
alexgill321 <alexgill321@gmail.com>
2023-11-10 14:36:51 -0700

    cov matrix calc

--- pythia_test.py
+++ pythia_test.py
@@ -1,10 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset
+from torch.utils.data import DataLoader
 import psutil
 
-# pile_dataset = load_dataset("monology/pile-uncopyrighted", streaming=True)
+pile_dataset = load_dataset("monology/pile-uncopyrighted", streaming=True)
 
-# print(next(iter(pile_dataset["train"])))
+pile_loader = DataLoader(pile_dataset["train"], batch_size=2)
+
+data_iter = iter(pile_loader)
+print(next(data_iter))
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-70m")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/pythia-70m")
