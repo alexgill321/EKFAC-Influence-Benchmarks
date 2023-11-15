f8aa778d38eb00de838ddfa8316c74c4a04a2628
Purbid Bambroo <purbidbambroo@Purbids-MacBook-Air.local>
2023-11-15 13:13:26 -0700

    pbrf over all examples

--- pbrf/calculate_pbrf_over_checkpoints.py
+++ pbrf/calculate_pbrf_over_checkpoints.py
@@ -10,16 +10,19 @@ torch.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-from src.linear_nn import load_data, get_model, load_model, test
+from src.linear_nn import load_data, get_model, load_model, test, load_checkpoint
 
 from pbrf.pbrf_helpers import calculate_bergman_divergance, pbrf_from_bergman
 
 train_loader, val_loader, test_loader = load_data()
 print("got the data.")
 model, criterion, optimizer = get_model()
+model1, criterion1, optimizer1 = get_model()
 
-untrained_model = load_model(model, filepath = 'models/checkpoints/checkpoint_1_linear_trained_model.pth')
-trained_model = load_model(model, filepath = 'models/linear_trained_model.pth')
+
+untrained_model, _, _, _= load_checkpoint(model, optimizer, filepath = 'models/checkpoints/checkpoint_1_linear_trained_model.pth')
+trained_model = load_model(model1, filepath = 'models/linear_trained_model.pth')
+
 print("got the model.")
 
 def compare_state_dicts(state_dict1, state_dict2):
@@ -34,8 +37,7 @@ state_dict2 = trained_model.state_dict()
 
 are_identical = compare_state_dicts(state_dict1, state_dict2)
 print("Are models identical?", are_identical)
-#
-# exit()
+
 # print(untrained_model.fc1.weight)
 # print("***")
 # print(trained_model.fc1.weight)
@@ -44,17 +46,23 @@ print("Are models identical?", are_identical)
 
 untrained_model_params = untrained_model.fc1.weight
 trained_model_params = trained_model.fc1.weight
-
-trained_model_params = trained_model_params*0.1
-
+print(untrained_model_params)
+print(trained_model_params)
+print(torch.eq(untrained_model_params,trained_model_params))
+diff_tensor = trained_model_params - untrained_model_params
+print(torch.sum(diff_tensor)) # to confirm the weights are not exactly the same
 
 training_preds_on_untrained_model, _ = test(untrained_model, '' , criterion, get_preds_only = True, train_loader = train_loader)
 training_preds_on_trained_model, output_grads = test(trained_model, '' , criterion, get_preds_only = True, train_loader = train_loader)
 
-one_example_bergman_cov = calculate_bergman_divergance(train_loader, training_preds_on_untrained_model,
+# one_example_bergman_cov = calculate_bergman_divergance(train_loader, training_preds_on_untrained_model,
+#                                                        training_preds_on_trained_model, criterion, output_grads)
+# pbrf_from_bergman(one_example_bergman_cov, untrained_model_params, trained_model_params)
+
+
+full_dataset_bergman_cov = calculate_bergman_divergance(train_loader, training_preds_on_untrained_model,
                                                        training_preds_on_trained_model, criterion, output_grads)
-pbrf_from_bergman(one_example_bergman_cov, untrained_model_params, trained_model_params)
+full_dataset_pbrf = pbrf_from_bergman(full_dataset_bergman_cov, untrained_model_params, trained_model_params)
 
-
-
+print(full_dataset_pbrf[:10])
 
--- pbrf/pbrf_helpers.py
+++ pbrf/pbrf_helpers.py
@@ -1,52 +1,59 @@
 import torch
+from tqdm import tqdm
 
 def calculate_bergman_divergance(trainloader, example_prediction, trained_model_example_prediction, criterion, output_grads):
-    # let's do only for one batch, hence break
-    for inputs, labels in trainloader:
 
+
+    bergman_divergence_for_entire_dataset = []
+    # let's do only for one batch, hence break
+
+    for batch_num, data_batch in enumerate(tqdm(trainloader)):
+    # for inputs, labels in trainloader:
+
+
+        inputs, labels = data_batch
         itr = 0
 
         for individual_inputs_in_batch, individual_label_in_batch in zip(inputs, labels):
 
-            loss_on_untrained_examples = criterion(example_prediction[0][itr].unsqueeze(dim=0),
+            loss_on_untrained_examples = criterion(example_prediction[batch_num][itr].unsqueeze(dim=0),
                                                    individual_label_in_batch.unsqueeze(dim=0))
 
-            loss_on_trained_examples = criterion(trained_model_example_prediction[0][itr].unsqueeze(dim=0),
+            loss_on_trained_examples = criterion(trained_model_example_prediction[batch_num][itr].unsqueeze(dim=0),
                                                  individual_label_in_batch.unsqueeze(dim=0))
 
-            output_grads[0] = output_grads[0].argmax(dim = 1).unsqueeze(dim = 1)
+            output_grads[batch_num] = output_grads[batch_num].argmax(dim = 1).unsqueeze(dim = 1)
 
-            example_pred_curr_with_argmax = example_prediction[0][itr].argmax(dim = 0)
-            # example_prediction[0][itr] = example_prediction[0][itr].argmax(dim = 0)
-            # print(example_prediction[0][itr].shape)
+            example_pred_curr_with_argmax = example_prediction[batch_num][itr].argmax(dim = 0)
             trained_model_example_pred_curr_with_argmax = trained_model_example_prediction[0][itr].argmax(dim = 0)
 
-            # trained_model_example_prediction[0][itr] = trained_model_example_prediction[0][itr].argmax(dim = 0)
-
-            # grad of final prediction wrt loss (not sure):
-            output_grads_colum_vector = output_grads[0][itr].t()
+            output_grads_colum_vector = output_grads[batch_num][itr].t()
 
             # difference_in_preds_before_vs_after_training
-            # output_difference_vector = example_prediction[0][itr] - trained_model_example_prediction[0][itr]
             output_difference_vector = example_pred_curr_with_argmax - trained_model_example_pred_curr_with_argmax
-
 
 
             output_matmul = output_grads_colum_vector * output_difference_vector
 
             final_bergman_for_current_input = loss_on_untrained_examples - loss_on_trained_examples - output_matmul
 
-            print(final_bergman_for_current_input)
+            bergman_divergence_for_entire_dataset.append(final_bergman_for_current_input)
 
             itr += 1
-            break
-        break
 
-    return final_bergman_for_current_input
+    return bergman_divergence_for_entire_dataset
 
 
 def pbrf_from_bergman(bergman_divergance, untrained_model_params,trained_model_params, ):
-    pbrf_for_curr_example = bergman_divergance.item() - torch.sum(
-        torch.square(untrained_model_params - trained_model_params))
+
+    full_dataset_pbrf = []
+    for each_example_divergence in bergman_divergance:
+        pbrf_for_curr_example = each_example_divergence.item() - torch.sum(
+            torch.square(untrained_model_params - trained_model_params))
+
+        full_dataset_pbrf.append(pbrf_for_curr_example)
+
+    return full_dataset_pbrf
+
 
 
