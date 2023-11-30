2eee500ead9e3a3d20c899c4c8939af06bac33bb
alexgill321 <alexgill321@gmail.com>
2023-11-30 13:16:22 -0700

    Some updated covariance calculation classes

--- src/influence_utils.py
+++ src/influence_utils.py
@@ -11,6 +11,9 @@ import torch.distributions as dist
 from torch.utils.data import DataLoader, Dataset
 from torch.optim.optimizer import Optimizer
 import tqdm
+import torch.nn as nn
+
+from matplotlib import pyplot as plt
 
 class EKFAC(Optimizer):
     def __init__(self, net, eps):
@@ -54,6 +57,37 @@ class EKFAC(Optimizer):
 
             self.calc_A(group, x)
             self.calc_S(group, gy)
+
+class Visualizer(Optimizer):
+    def __init__(self, net, eps):
+        self.eps = eps
+        self.params = []
+        self._fwd_handles = []
+        self._bwd_handles = []
+        self.net = net
+        self.calc_act = True
+    
+        for mod in net.modules():
+            mod_class = mod.__class__.__name__
+            if mod_class in ['Linear']:
+                handle = mod.register_forward_pre_hook(self._save_input)
+                self._fwd_handles.append(handle)
+                handle = mod.register_full_backward_hook(self._save_grad_output)
+                self._bwd_handles.append(handle)
+                params = [mod.weight]
+                if mod.bias is not None:
+                    params.append(mod.bias)
+                d = {'params': params, 'mod': mod, 'layer_type': mod_class}
+                self.params.append(d)
+        super(Visualizer, self).__init__(self.params, {})
+
+    def step(self):
+        for group in self.param_groups:
+            mod = group['mod']
+            x = self.state[mod]['x']
+            gy = self.state[mod]['gy']
+
+            plt.scatter(x)
         
     def calc_A(self, group, x):
         if self.calc_act:
@@ -384,3 +418,97 @@ class EKFACInfluence(DataInfluence):
 
             
         return G_list
+
+class ComputeCovA:
+
+    @classmethod
+    def compute_cov_a(cls, a, layer):
+        return cls.__call__(a, layer)
+
+    @classmethod
+    def __call__(cls, a, layer):
+        if isinstance(layer, nn.Linear):
+            cov_a = cls.linear(a, layer)
+        elif isinstance(layer, nn.Conv2d):
+            # cov_a = cls.conv2d(a, layer)
+            cov_a = None
+        else:
+            # FIXME(CW): for extension to other layers.
+            # raise NotImplementedError
+            cov_a = None
+
+        return cov_a
+
+    # @staticmethod
+    # def conv2d(a, layer):
+    #     batch_size = a.size(0)
+    #     a = _extract_patches(a, layer.kernel_size, layer.stride, layer.padding)
+    #     spatial_size = a.size(1) * a.size(2)
+    #     a = a.view(-1, a.size(-1))
+    #     if layer.bias is not None:
+    #         a = torch.cat([a, a.new(a.size(0), 1).fill_(1)], 1)
+    #     a = a/spatial_size
+    #     # FIXME(CW): do we need to divide the output feature map's size?
+    #     return a.t() @ (a / batch_size)
+
+    @staticmethod
+    def linear(a, layer):
+        # a: batch_size * in_dim
+        batch_size = a.size(0)
+        if layer.bias is not None:
+            a = torch.cat([a, a.new(a.size(0), 1).fill_(1)], 1)
+        return a.t() @ (a / batch_size)
+
+
+class ComputeCovG:
+
+    @classmethod
+    def compute_cov_g(cls, g, layer, batch_averaged=False):
+        """
+        :param g: gradient
+        :param layer: the corresponding layer
+        :param batch_averaged: if the gradient is already averaged with the batch size?
+        :return:
+        """
+        # batch_size = g.size(0)
+        return cls.__call__(g, layer, batch_averaged)
+
+    @classmethod
+    def __call__(cls, g, layer, batch_averaged):
+        if isinstance(layer, nn.Conv2d):
+            # cov_g = cls.conv2d(g, layer, batch_averaged)
+            cov_g = None
+        elif isinstance(layer, nn.Linear):
+            cov_g = cls.linear(g, layer, batch_averaged)
+        else:
+            cov_g = None
+
+        return cov_g
+
+    # @staticmethod
+    # def conv2d(g, layer, batch_averaged):
+    #     # g: batch_size * n_filters * out_h * out_w
+    #     # n_filters is actually the output dimension (analogous to Linear layer)
+    #     spatial_size = g.size(2) * g.size(3)
+    #     batch_size = g.shape[0]
+    #     g = g.transpose(1, 2).transpose(2, 3)
+    #     g = try_contiguous(g)
+    #     g = g.view(-1, g.size(-1))
+
+    #     if batch_averaged:
+    #         g = g * batch_size
+    #     g = g * spatial_size
+    #     cov_g = g.t() @ (g / g.size(0))
+
+    #     return cov_g
+
+    @staticmethod
+    def linear(g, layer, batch_averaged):
+        # g: batch_size * out_dim
+        batch_size = g.size(0)
+
+        if batch_averaged:
+            cov_g = g.t() @ (g * batch_size)
+        else:
+            cov_g = g.t() @ (g / batch_size)
+        return cov_g
