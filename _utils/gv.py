b88009377b6509a07c532b48da84622a6362963e
alexgill321 <alexgill321@gmail.com>
2023-12-30 15:30:23 -0800

    implementation for gv utility (untested)

--- _utils/gv.py
+++ _utils/gv.py
@@ -8,7 +8,7 @@ from typing import Any, List, Optional, Tuple, Union
 
 import captum._utils.common as common
 import torch
-from captum.attr import LayerActivation
+from captum._utils.gradient import compute_layer_gradients_and_eval
 from torch import Tensor
 from torch.nn import Module
 from torch.utils.data import DataLoader, Dataset
@@ -162,6 +162,56 @@ class GV:
         return os.path.exists(gv_dir) and len(glob.glob(gv_filesearch)) > 0
     
     @staticmethod
+    def save(
+        path: str,
+        model_id: str,
+        identifier: str,
+        layers: Union[str, List[str]],
+        grad_tensors: Union[Tensor, List[Tensor]],
+        num_id: str,
+    ) -> None:
+        r"""
+        Saves the gradient vectors for the given `model_id` and
+        `layer` to the `path`.
+
+        Args:
+            path (str): The path where the gradient vectors
+                    for the `model_id` are stored.
+            model_id (str): The name/version of the model for which layer gradients
+                    are being computed and stored.
+            identifier (str): An identifier for the layer gradients.
+                    Can be used to distinguish between gradients for different
+                    training batches.
+            layer (str): The layer for which the gradient vectors are computed
+            grad_tensors (Tensor): A batch of gradient vectors. This must match the
+                    dimension of 'layers'.
+            num_id (str): An optional string representing the batch number for which
+                the gradient vectors are computed.
+
+        Returns:
+            None
+        """
+        if isinstance(layers, str):
+            layers = [layers]
+        if isinstance(grad_tensors, Tensor):
+            grad_tensors = [grad_tensors]
+
+        if len(layers) != len(grad_tensors):
+            raise ValueError("The dimension of `layers` and `grad_tensors` must match!")
+
+        gv_dir = GV._assemble_model_dir(path, model_id)
+
+        for i, layer in enumerate(layers):
+            gv_save_fl_path = os.path.join(
+                GV._assemble_file_path(gv_dir, identifier, layer), "%s.pt" % num_id
+            )
+
+            layer_dir = os.path.dirname(gv_save_fl_path)
+            if not os.path.exists(layer_dir):
+                os.makedirs(layer_dir)
+            torch.save(grad_tensors[i], gv_save_fl_path)
+
+    @staticmethod
     def load(
         path: str,
         model_id: str,
@@ -264,6 +314,8 @@ class GV:
         layers: Union[str, List[str]],
         inputs: Union[Tensor, Tuple[Tensor, ...]],
         identifier: str,
+        targets: Optional[Union[Tensor, Tuple[Tensor, ...]]] = None,
+        criterion: Optional[torch.nn.Module] = None,
         num_id: Optional[str] = None,
         additional_forward_args: Any = None,
         load_from_disk: bool = True,
@@ -275,13 +327,142 @@ class GV:
             path (str): The path where the gradient vectors
                     for the `layer` are stored.
             model (torch.nn.Module): An instance of pytorch model. This model should
-                define all of its layers as attributes of the model.
+                    define all of its layers as attributes of the model.
             model_id (str): The name/version of the model for which layer gradients
-                    are being computed and stored.
+                        are being computed and stored.
             layers (str or List[str]): The layers for which the gradient vectors
                 are computed.
             inputs (torch.Tensor or Tuple[torch.Tensor, ...]): The inputs to
-                the model.
+                    the model for which the gradients are computed. The first dimension in
+                    the 'inputs' tensor should represent the number of examples.
+            identifier (str or None): An optional identifier for the layer gradients.
+                    Can be used to distinguish between gradients for different
+                    training batches.
+            targets (torch.Tensor or Tuple[torch.Tensor, ...], optional): The targets
+                    for the inputs. If provided, a criterion must also be provided.
+            criterion (torch.nn.Module, optional): The criterion used to compute
+                    the loss.
+            num_id (str): An optional string representing the batch number for which
+                    the gradient vectors are computed.
+            additional_forward_args (Any, optional): Any additional arguments that will be
+                    passed to 'model' after inputs.
+            load_from_disk (bool): Forces function to regenerate gradients if False.
         """
 
-        # TODO: Write this method for influence utils
+        unsaved_layers = GV._manage_loading_layers(
+            path,
+            model_id,
+            layers,
+            load_from_disk,
+            identifier,
+            num_id
+        )
+        layer_modules = [
+            common._get_module_from_name(model, layer) for layer in unsaved_layers
+        ]
+
+        def forward_with_loss(inputs, model, criterion, targets):
+                outputs = model(inputs)
+                loss = criterion(outputs, targets) if criterion and targets is not None else None
+                return loss
+
+        if criterion is not None:
+            if additional_forward_args is None:
+                additional_forward_args = (targets, )
+            else:
+                additional_forward_args = (targets, ) + tuple(additional_forward_args)
+
+            new_grads, _ = compute_layer_gradients_and_eval(
+                forward_with_loss,
+                layer_modules,
+                inputs,
+                additional_forward_args
+            )
+        else:
+            new_grads, _ = compute_layer_gradients_and_eval(
+                model,
+                layer_modules,
+                inputs,
+                additional_forward_args
+            )
+
+        GV.save(path, model_id, identifier, unsaved_layers, new_grads, num_id)
+
+    @staticmethod
+    def _unpack_data(data: Union[Any, Tuple[Any, Any]]) -> Any:
+        if isinstance(data, tuple) or isinstance(data, list):
+            return data[0]
+        else:
+            return data
+
+    @staticmethod
+    def generate_dataset_gradients(
+        path: str,
+        model: Module,
+        model_id: str,
+        layers: Union[str, List[str]],
+        dataloader: DataLoader,
+        criterion: Optional[torch.nn.Module] = None,
+        identifier: str = "default",
+        load_from_disk: bool = True,
+        return_grads: bool = False,
+    ) -> Optional[Union[GVDataset, List[GVDataset]]]:
+        r"""
+        Computes layer gradients for a source dataset and specified 'layers'. Assumes
+        that the dataset returns a single value, or a tuple containing two elements.
+
+        Args: 
+            path (str): The path where the gradient vectors for the `layer` are stored.
+            model (torch.nn.Module): An instance of pytorch model. This model should
+                    define all of its layers as attributes of the model.
+            model_id (str): The name/version of the model for which layer gradients
+                        are being computed and stored.
+            layers (str or List[str]): The layer(s) for which the gradient vectors
+                are computed.
+            dataloader (torch.utils.data.DataLoader): The source dataset for which the
+                    gradients are computed.
+            criterion (torch.nn.Module, optional): The criterion used to compute the loss.
+            identifier (str, optional): An optional identifier for the layer gradients.
+                    Can be used to distinguish between gradients for different
+                    training batches.
+                    Default: "default"
+            load_from_disk (bool): Forces function to regenerate gradients if False.
+                    Default: True
+            return_grads (bool, optional): Whether to return the gradients.
+                    Default: False
+            
+        Returns: If `return_grads == True`, returns a single `GVDataset` if `layers` is a 
+                str, otherwise, a list of `GVDataset`s of the length of `layers`, where each
+                element corresponds to a layer.  In either case, `GVDataset`'s represent the
+                gradients for a single layer, over the entire `dataloader`.  If `return_grads
+                == False`, does not return anything.
+
+        """
+
+        unsaved_layers = GV._manage_loading_layers(
+            path,
+            model_id,
+            layers,
+            load_from_disk,
+            identifier,
+        )
+        if len(unsaved_layers) > 0:
+            for i, data in enumerate(dataloader):
+                GV._compute_and_save_gradients(
+                    path,
+                    model,
+                    model_id,
+                    layers,
+                    GV._unpack_data(data), #TODO: make unpack data work with targets
+                    identifier,
+                    criterion,
+                    str(i),
+                )
+        
+        if not return_grads:
+            return None
+        if isinstance(layers, str):
+            return GV.load(path, model_id, identifier, layers)
+        else:
+            return [GV.load(path, model_id, identifier, layer) for layer in layers]
+            
