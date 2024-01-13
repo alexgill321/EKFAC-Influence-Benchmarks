1e7be34ba066098190dc8562fe0d9711ac3c1bb8
alexgill321 <alexgill321@gmail.com>
2024-01-12 20:27:28 -0700

    Lissa Script updates

--- src/mnist_lissa.py
+++ src/mnist_lissa.py
@@ -2,7 +2,11 @@ from torch_influence import BaseObjective, LissaInfluenceModule
 from torchvision import datasets, transforms
 import torch
 from linear_nn import get_model, load_model
-from torch.utils.data import Subset
+from torch.utils.data import Subset, DataLoader
+import os
+from tqdm import tqdm
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
     net, _, _= get_model()
@@ -33,7 +37,26 @@ def main():
             outputs = model(batch[0])
             criterion = torch.nn.CrossEntropyLoss()
             return criterion(outputs, batch[1])
+        
+    module = LissaInfluenceModule(
+        model = model,
+        objective = ClassObjective(),
+        train_loader = DataLoader(train_dataset, batch_size=32),
+        test_loader = DataLoader(test_dataset, batch_size=32),
+        device=DEVICE,
+        damp=1e-4,
+        repeat=10,
+        depth=5000,
+        scale=.95,
+        gnh=True
+    )
     
+    train_idxs = list(range(train_dataset.shape[0]))
+    test_idxs = list(range(test_dataset.shape[0]))
+
+    for test_idx in tqdm(test_idxs, desc="Computing Influences"):
+        module.compute_influence_on_test_loss(test_idx, train_idxs)
+        module.compute_influence_on_train_loss(test_idx, train_idxs)
 
 
 
