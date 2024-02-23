da6f9b0fe0080defe23bcb1f5c1d57011d208496
alexgill321 <alexgill321@gmail.com>
2024-02-22 17:05:05 -0700

    chpc update

--- download_pile_chpc.py
+++ download_pile_chpc.py
@@ -1,7 +1,7 @@
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
-hf_hub_download(
+snapshot_download(
     repo_id="EleutherAI/pile-standard-pythia-preshuffled", 
     repo_type="dataset", 
-    cache_dir="/scratch/general/u1380656/pile", 
+    cache_dir="/scratch/general/u1380656/pile"
 )
