3e1d2595b048472cb68b149328085d49000ba8e6
alexgill321 <alexgill321@gmail.com>
2024-02-23 12:24:14 -0700

    Update job.sh

--- job.sh
+++ job.sh
@@ -7,10 +7,11 @@
 #SBATCH --time=2:00:00
 
 WORKDIR=$HOME/pythia
+VENVDIR=$HOME/EKFAC-Influence-Benchmarks
 SCRDIR=/scratch/general/vast/$USER/pile
 INPUTDIR=$SCRDIR/datasets--EleutherAI--pile-standard-pythia-preshuffled/snapshots/bac79b6820adb34e451f9a02cc1dc7cd920febf0/document-00000-of-00020.bin
 
-source $WORKDIR/ekfac/bin/activate
+source $VENVDIR/ekfac/bin/activate
 
 python $WORKDIR/utils/unshard_memmap.py --input_file $INPUTDIR --num_shards 21 --output_dir $SCRDIR
 
