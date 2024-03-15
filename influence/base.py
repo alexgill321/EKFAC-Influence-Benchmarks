Fixed GPU accumulation for cov calculation

influence/base.py
@@ -279,7 +279,7 @@ class BaseLayerInfluenceModule(BaseInfluenceModule):
                     scores[layer_name] = (layer_grad @ ihvps[layer_name]).view(-1, 1)
                 else:
                     scores[layer_name] = torch.cat([scores[layer_name], (layer_grad @ ihvps[layer_name]).view(-1, 1)], dim=1)
-                
+         
         return scores
     
     def _compute_ihvps(self, test_idxs: List[int]) -> torch.Tensor:
@@ -417,31 +417,32 @@ class BaseKFACInfluenceModule(BaseLayerInfluenceModule):
     
     def _update_covs(self):
         for layer_name, layer in zip(self.layer_names, self.layer_modules):
-            x = self.state[layer]['x']
-            gy = self.state[layer]['gy']
-            
-            if x.dim() == 2:
-                x = x.data.t()
-                gy = gy.data.t()
+            with torch.no_grad():
+                x = self.state[layer]['x'].detach()
+                gy = self.state[layer]['gy'].detach()
+                
+                if x.dim() == 2:
+                    x = x.data.t()
+                    gy = gy.data.t()
 
-                if layer.bias is not None:
-                    ones = torch.ones_like(x[:1])
-                    x = torch.cat([x, ones], dim=0)
+                    if layer.bias is not None:
+                        ones = torch.ones_like(x[:1])
+                        x = torch.cat([x, ones], dim=0)
 
-            """
-            x dimensions (before permute):
+                """
+                x dimensions (before permute):
 
-            0: batch size
-            1: sequence length
-            2: hidden size
-            """
-            if x.dim() == 3:
-                x = x.permute(0, 2, 1)
-                gy = gy.permute(0, 2, 1)
-                if layer.bias is not None:
-                    x = torch.cat([x, torch.ones_like(x[:, :1])], dim=1)
+                0: batch size
+                1: sequence length
+                2: hidden size
+                """
+                if x.dim() == 3:
+                    x = x.permute(0, 2, 1)
+                    gy = gy.permute(0, 2, 1)
+                    if layer.bias is not None:
+                        x = torch.cat([x, torch.ones_like(x[:, :1])], dim=1)
 
-            self._calc_covs(layer_name, x, gy)
+                self._calc_covs(layer_name, x, gy)
     
     def _calc_covs(self, layer, x, gy):
         if x.dim == 2:

influence/modules.py
@@ -66,13 +66,24 @@ class EKFACInfluenceModule(BaseKFACInfluenceModule):
         cov_batched = tqdm(self.cov_loader, total=len(self.cov_loader), desc="Calculating Covariances")
 
         for batch in cov_batched:
-            loss = self.objective.pseudograd_loss(self.model, batch, n_samples=self.n_samples, generator=self.generator)
-            for l in loss:
-                l.backward(retain_graph=True)
+            losses = self.objective.pseudograd_loss(self.model, batch, n_samples=self.n_samples, generator=self.generator)
+            try:
+                current_loss = next(losses)
+            except StopIteration:
+                # Handle case where cov_batched is empty
+                current_loss = None
+            while current_loss is not None:
+                try:
+                    next_loss = next(losses)
+                    retain_graph = True
+                except StopIteration:
+                    next_loss = None
+                    retain_graph = False
+                current_loss.backward(retain_graph=retain_graph)
                 self._update_covs()
                 self.model.zero_grad()
-                torch.cuda.empty_cache()
-            del loss
+                current_loss = next_loss
+            torch.cuda.empty_cache()
 
         # May have to change based on intended batching
         for layer in self.layer_names: